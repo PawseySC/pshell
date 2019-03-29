395067af650a388ca69014c32c1fcd1244145b5a
Sean Fleming <sean.fleming@pawsey.org.au>
2019-03-29 11:42:50 +0800

    Reworked to make this test suitable for testing zeus pshell module deployment (and pshell_bundle)

--- test_pshell.py
+++ test_pshell.py
@@ -5,53 +5,51 @@ import sys
 import unittest
 import subprocess
 import xml.etree.ElementTree as ET
-import pshell
 
 #########################
 # serverless pshell tests
 #########################
 class pshell_syntax(unittest.TestCase):
 
+#    pshell_exe = "pshell.py"
+    pshell_exe = "pshell"
+
     def test_cd(self):
-        proc = subprocess.Popen(["pshell.py", "-c", "dummy", "cd /projects\"'"], stdout=subprocess.PIPE)
+        proc = subprocess.Popen([self.pshell_exe, "-c", "dummy", "cd /projects\"'"], stdout=subprocess.PIPE)
         for line in proc.stdout:
             if "request" in line:
                 self.assertEqual(line.strip(), '<request><service name="service.execute" session=""><args><service name="asset.namespace.exists"><namespace>/projects"\'</namespace></service></args></service></request>')
 
     def test_rm(self):
-        proc = subprocess.Popen(["pshell.py", "-c", "dummy", "rm *\'*"], stdout=subprocess.PIPE)
+        proc = subprocess.Popen([self.pshell_exe, "-c", "dummy", "rm *\'*"], stdout=subprocess.PIPE)
         for line in proc.stdout:
             if "request" in line:
                 self.assertEqual(line.strip(), '<request><service name="service.execute" session=""><args><service name="asset.query"><where>namespace=\'/projects\' and name=\'*\\\'*\'</where><action>count</action></service></args></service></request>')
 
     def test_file(self):
-        proc = subprocess.Popen(["pshell.py", "-c", "dummy", r'file "/dir1/../dir2/test_!@#\""'], stdout=subprocess.PIPE)
+        proc = subprocess.Popen([self.pshell_exe, "-c", "dummy", r'file "/dir1/../dir2/test_!@#\""'], stdout=subprocess.PIPE)
         for line in proc.stdout:
             if "request" in line:
                 self.assertEqual(line.strip(), '<request><service name="service.execute" session=""><args><service name="asset.get"><id>path=/dir2/test_!@#"</id></service></args></service></request>')
 
     def test_mkdir(self):
-        proc = subprocess.Popen(["pshell.py", "-c", "dummy", "mkdir /dir1/../dir2/namespace\"'"], stdout=subprocess.PIPE)
+        proc = subprocess.Popen([self.pshell_exe, "-c", "dummy", "mkdir /dir1/../dir2/namespace\"'"], stdout=subprocess.PIPE)
         for line in proc.stdout:
             if "request" in line:
                 self.assertEqual(line.strip(), '<request><service name="service.execute" session=""><args><service name="asset.namespace.create"><namespace>/dir2/namespace"\'</namespace></service></args></service></request>')
 
     def test_rmdir(self):
-        proc = subprocess.Popen(["pshell.py", "-c", "dummy", "rmdir sean's dir"], stdout=subprocess.PIPE)
+        proc = subprocess.Popen([self.pshell_exe, "-c", "dummy", "rmdir sean's dir"], stdout=subprocess.PIPE)
         for line in proc.stdout:
             if "request" in line:
                 self.assertEqual(line.strip(), '<request><service name="service.execute" session=""><args><service name="asset.namespace.exists"><namespace>/projects/sean\'s dir</namespace></service></args></service></request>')
 
-# NEW - test conversion of XML to Arcitecta's shorthand format - used for metadata import
-    def test_import_metadata(self):
-        pp = pshell.parser()
-        xml_root = ET.fromstring('<ctype>"image/tiff"</ctype>')
-        result = pp.xml_to_mf(xml_root, result=None)
-        self.assertEqual(result, ' :ctype "image/tiff"')
-        xml_root = ET.fromstring('<geoshape><point><elevation>5.0</elevation></point></geoshape>')
-        result = pp.xml_to_mf(xml_root, result=None)
-        self.assertEqual(result, ' :geoshape < :point < :elevation 5.0 > >')
-
+# expected response from server is "invalid session" ... anything else is a server connection problem
+    def test_server_responding(self):
+        proc = subprocess.Popen(["pshell", "whoami"], stdout=subprocess.PIPE)
+        for line in proc.stdout:
+            if "Failed to establish network connection" in line:
+                raise Exception("Server failed to respond.")
 
 ########################################
 # convenience wrapper for squishing bugs
@@ -60,11 +58,17 @@ class pshell_bugs(unittest.TestCase):
     def setUp(self):
         print "setup"
 
-# TODO - more testing of pshell 
-    def test_squish(self):
-        proc = subprocess.Popen(["pshell.py", "-c", "dummy", "import script2"], stdout=subprocess.PIPE)
-        for line in proc.stdout:
-            print line.strip()
+# this works ... but is slow ... takes 7+ seconds ...
+#    def test_public_www(self):
+#        proc = subprocess.Popen(["pshell", "get /www/index.html"], stdout=subprocess.PIPE)
+#        for line in proc.stdout:
+#            print line.strip()
+
+# can't do - as we forbid login in a pshell scripted sense ...
+#    def test_connection(self):
+#        proc = subprocess.Popen(["pshell", "login"], stdout=subprocess.PIPE)
+#        for line in proc.stdout:
+#            print line.strip()
 
 ######
 # main
@@ -77,7 +81,6 @@ if __name__ == '__main__':
 
 # classes to test
     test_class_list = [pshell_syntax]
-
 #    test_class_list = [pshell_bugs]
 
 
